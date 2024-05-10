53814f7b12666e6eaf214661844b72e185fab727
kaiyuanCui <kevincui2002@gmail.com>
2024-05-11 02:52:18 +1000

    minor modification to data_upload.py to enable more flexibility

--- sudo/data_upload.py
+++ sudo/data_upload.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import requests
 
+INDEX_NAME = 'economy'
+
 
 url = 'https://localhost:9200'
 headers = {'Content-Type': 'application/json'}
@@ -11,6 +13,7 @@ verify_ssl = False
 
 # Process Homeless Data
 def merge_data(data_dir):
+    all_data_df = None
     for file_name in os.listdir(data_dir):
         if file_name.endswith('.csv'):
             # Read each CSV file into a DataFrame
@@ -18,8 +21,11 @@ def merge_data(data_dir):
             df = pd.read_csv(file_path)
             # Concatenate to the main DataFrame by appending rows
             all_data_df = pd.concat([all_data_df, df], ignore_index=True)
-    all_data_df[' fin_yr'] = all_data_df[' fin_yr'].str[:4]
-    all_data_df[' lga_name'] = all_data_df[' lga_name'].astype(str)
+
+    if ' fin_yr' in all_data_df:      
+        all_data_df[' fin_yr'] = all_data_df[' fin_yr'].str[:4]
+    if ' lga_name' in all_data_df:
+        all_data_df[' lga_name'] = all_data_df[' lga_name'].astype(str)
     return all_data_df
 
 # Uplaod to Elastic Search
@@ -41,7 +47,7 @@ def upload_es(index_name, data_dir):
     return True
 
 def main():
-    upload_es('homeless', './data')
+    upload_es(INDEX_NAME, './data')
 
 if __name__ == '__main__':
     main()
